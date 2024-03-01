cf6464fcf25a00864aae236f1c7a4a59b43d5a3c
Daniman30 <regulardani1@gmail.com>
2024-03-01 11:39:39 -0500

    littles updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import {RouterModule} from '@angular/router'
+import {AdminComponent} from './admin/admin.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, SignupComponent, RouterModule],
+  imports: [RouterOutlet, LoginComponent, SignupComponent, RouterModule, AdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
