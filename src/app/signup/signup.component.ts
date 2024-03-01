7ec1d72ac3639160008688fa5a027998eac5176e
Daniman30 <regulardani1@gmail.com>
2024-03-01 04:09:52 -0500

    links arreglados

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RouterOutlet } from '@angular/router';
+import {RouterModule} from '@angular/router'
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, LoginComponent, SignupComponent, RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
