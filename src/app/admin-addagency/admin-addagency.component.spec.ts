69178eb7e70ccfe3b34b5e9d27c5e2a28847f9da
Adrián <86851123+adrianfoya@users.noreply.github.com>
2024-03-18 19:20:40 -0400

    admin menu

---
+++ src/app/admin-addagency/admin-addagency.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminAddagencyComponent } from './admin-addagency.component';
+
+describe('AdminAddagencyComponent', () => {
+  let component: AdminAddagencyComponent;
+  let fixture: ComponentFixture<AdminAddagencyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AdminAddagencyComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AdminAddagencyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
