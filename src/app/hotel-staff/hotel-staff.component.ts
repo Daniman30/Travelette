ef2ab54969c0e8b04960e30dd2444eee1d0831c2
Daniman30 <regulardani1@gmail.com>
2024-03-27 00:10:25 -0400

    Fix 'cositas'

---
+++ src/app/hotel-staff/hotel-staff.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-hotel-staff',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './hotel-staff.component.html',
+  styleUrl: './hotel-staff.component.css'
+})
+export class HotelStaffComponent {
+
+}
